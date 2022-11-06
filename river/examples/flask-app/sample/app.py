d79d3402bc1fb5cd81c2343757ea39634c47cc1b
Masato Naka <masatonaka1989@gmail.com>
2022-11-06 16:42:29 +0900

    upgrade flask

--- river/examples/flask-app/sample/app.py
+++ river/examples/flask-app/sample/app.py
@@ -7,34 +7,33 @@ from river import compose, linear_model, preprocessing
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def predict():
     payload = request.json
     river_model = load_model()
     return river_model.predict_proba_one(payload)
 
 
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["POST"])
 def learn():
     payload = request.json
     river_model = load_model()
-    river_model.learn_one(payload['features'], payload['target'])
+    river_model.learn_one(payload["features"], payload["target"])
     save_model(river_model)
     return {}, 201
 
 
 def new_model():
     model = compose.Pipeline(
-        preprocessing.StandardScaler(),
-        linear_model.LogisticRegression()
+        preprocessing.StandardScaler(), linear_model.LogisticRegression()
     )
     return model
 
 
 def load_model():
-    model_file = 'model.pkl'
+    model_file = "model.pkl"
     if exists(model_file):
-        with open(model_file, 'rb') as f:
+        with open(model_file, "rb") as f:
             return pickle.load(f)
     else:
         model = new_model()
@@ -43,8 +42,9 @@ def load_model():
 
 
 def save_model(model):
-    with open('model.pkl', 'wb') as f:
+    with open("model.pkl", "wb") as f:
         pickle.dump(model, f)
 
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
